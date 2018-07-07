8b352c80d8806956336b7cc01532b9e2f8c48039
Dreamersoul <dreamersoul1@gmail.com>
2018-07-07 17:03:56 +0300

    This closes #4

--- lib/administrate/field/active_storage.rb
+++ lib/administrate/field/active_storage.rb
@@ -6,16 +6,22 @@ module Administrate
     class ActiveStorage < Administrate::Field::Base
       class Engine < ::Rails::Engine
       end
-      # currently we are using Rails.application.routes.url_helpers 
-      # without including the namespace because it runs into an
-      # exception
+
+      def url_only?
+        options.fetch(:url_only, false)
+      end
+
       def many?
         # find a way to use instance_of
         data.class.name == "ActiveStorage::Attached::Many"
       end
+
+      # currently we are using Rails.application.routes.url_helpers 
+      # without including the namespace because it runs into an
+      # exception
+
       # work around since calling data.preview(options)
       # returns "/images/<ActiveStorage::Preview>" which isnt the url
-
       def preview(attachment, options)
         Rails.application.routes.url_helpers.rails_representation_path(attachment.preview(options), only_path: true)
       end
