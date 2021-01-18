c26912860486897babce860d4802fb5408100b9a
Sebastien Dubois <sedubois@users.noreply.github.com>
2021-01-18 12:41:46 +0100

    Remove example-project

--- example-project/app/controllers/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class UsersController < ApplicationController
-    def destroy_avatar
-        avatar = requested_resource.avatars.find(params[:attachment_id])
-        avatar.purge
-        redirect_back(fallback_location: requested_resource)
-    end
-end
--- example-project/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ApplicationRecord
-    has_many_attached :avatars
-end
