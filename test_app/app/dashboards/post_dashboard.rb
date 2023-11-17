490e907ba15a5db78cc9d89921d1b6709252a8af
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-11-17 14:53:31 -0800

    Add post title to Administrate dashboard

--- test_app/app/dashboards/post_dashboard.rb
+++ test_app/app/dashboards/post_dashboard.rb
@@ -10,6 +10,7 @@ class PostDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     id: Field::Number,
     cover_image: Field::ActiveStorage,
+    title: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
   }.freeze
@@ -21,6 +22,7 @@ class PostDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
   id
+  title
   cover_image
   created_at
   ].freeze
@@ -29,6 +31,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
   id
+  title
   cover_image
   created_at
   updated_at
@@ -38,6 +41,7 @@ class PostDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+  title
   cover_image
   ].freeze
 
