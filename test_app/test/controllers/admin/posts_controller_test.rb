d64846876925405bfad48f8ec5f4296e6d7ecaa7
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-11-17 14:32:50 -0800

    Add new, create specs for posts controller

--- test_app/test/controllers/admin/posts_controller_test.rb
+++ test_app/test/controllers/admin/posts_controller_test.rb
@@ -10,5 +10,25 @@ module Admin
       assert_response :ok
       assert_select "td.cell-data--active-storage > a[href='#{admin_post_path(post)}']"
     end
+
+    test "new" do
+      get new_admin_post_path
+
+      assert_response :ok
+    end
+
+    test "create" do
+      file = fixture_file_upload("cover_image.jpg")
+
+      assert_difference "Post.count", 1 do
+        post admin_posts_path, params: {
+          post: {
+            cover_image: file
+          }
+        }
+      end
+
+      assert_response :redirect
+    end
   end
 end
