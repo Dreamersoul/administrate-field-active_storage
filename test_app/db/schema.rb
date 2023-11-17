0c16117a2ff9be612adad5d2fc09ea1d18665420
Jeanine Soterwood <jeanine@thoughtbot.com>
2023-11-17 14:50:17 -0800

    Add title field to posts

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_122927) do
+ActiveRecord::Schema.define(version: 2023_11_17_224946) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_122927) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
