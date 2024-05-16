5e906c4ed81a24765e5fdbe1d3dd02f70ae78529
Quinn Daley <quinn@fishpercolator.co.uk>
2024-05-16 10:08:54 +0100

    Fix behaviour for index_display_preview to match documentation

--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_17_224946) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_17_224946) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,9 +40,9 @@ ActiveRecord::Schema.define(version: 2023_11_17_224946) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
